036bc44789c1f7d7acd4ebfea95a37666728c73e
hvtk <hvtk1968@gmail.com>
2021-04-13 14:44:21 +0200

    added pages and routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route,} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-    Hallo allemaal
-    </div>
+      <Router>
+        <Switch>
+           <Route exact path="/">
+            <Homepage />
+           </Route>
+           <Route path="/sign-in">
+            <loginpagine />
+           </Route>
+           <Route path="/profile">
+            <profielpagina />
+           </Route>
+           <Route path="/sign-up">
+            <registreerpagina />
+           </Route>
+        </Switch>
+      </Router>
   );
 }
 
